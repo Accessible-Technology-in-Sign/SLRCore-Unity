59765ed77c319590ca483c0c6eb066a7bc0a755e
heirtoturnabout <nick.unger08@gmail.com>
2024-10-14 10:55:09 -0400

    Current skeleton app

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Mediapipe;
 using Mediapipe.Tasks.Vision.HandLandmarker;
 using Mediapipe.Unity;
+using TMPro;
 using RunningMode = Mediapipe.Tasks.Vision.Core.RunningMode;
 
-public class SimpleSLR : MonoBehaviour {
+public class SimpleSLR : MonoBehaviour
+{
     private SLRTfLiteModel<string> recognizer;
     private Buffer<HandLandmarkerResult> buffer;
     public MediapipeHandModelManager posePredictor;
@@ -16,22 +19,43 @@ public class SimpleSLR : MonoBehaviour {
     [SerializeField] private TextAsset _mediapipeGraph;
     [SerializeField] private bool _isGPU;
     [SerializeField] private bool _isInterpolating;
+    [SerializeField] private TextMeshProUGUI output;
+    [SerializeField] private Button trigger;
+    private string lastTranslation = "";
 
     private class Config
     {
         public static readonly int NUM_INPUT_FRAMES = 60;
         public static readonly int NUM_INPUT_POINTS = 21;
     }
-    
+
+    void Update()
+    {
+        output.text = lastTranslation;
+        trigger.onClick.AddListener(TriggerOnClick);
+
+    }
+
+    void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+    }
+
     void Awake()
     {
         buffer = new Buffer<HandLandmarkerResult>();
         string[] mapping = _mappingFile.text.Split("\n");
-        for (int i = 0; i < mapping.Length; i++) {
+
+        for (int i = 0; i < mapping.Length; i++)
+        {
             mapping[i] = mapping[i].Trim().ToLower();
-        } 
+        }
+
+
         recognizer = new SLRTfLiteModel<string>(_modelFile, mapping);
         posePredictor = new MediapipeHandModelManager(_mediapipeGraph.bytes, RunningMode.LIVE_STREAM);
+
         posePredictor.AddCallback("buffer", result =>
         {
             buffer.AddElement(result.Result);
@@ -40,64 +64,83 @@ public class SimpleSLR : MonoBehaviour {
         {
             List<float> inputArray = new List<float>();
 
-        if (_isInterpolating && bufferedResults.Count < Config.NUM_INPUT_FRAMES && bufferedResults.Count > 0)
-        {
-            foreach (var landmark in bufferedResults)
+            if (_isInterpolating && bufferedResults.Count < Config.NUM_INPUT_FRAMES && bufferedResults.Count > 0)
             {
-                for (int j = 0; j < Config.NUM_INPUT_POINTS; j++)
+                foreach (var landmark in bufferedResults)
                 {
-                    if (landmark.handLandmarks.Count <= 0 || landmark.handLandmarks[0].landmarks.Count <= 0)
-                        return; // Exit if no landmark is present
+                    for (int j = 0; j < Config.NUM_INPUT_POINTS; j++)
+                    {
+                        if (landmark.handLandmarks.Count <= 0 || landmark.handLandmarks[0].landmarks.Count <= 0)
+                            return; // Exit if no landmark is present
 
-                    inputArray.Add(landmark.handLandmarks[0].landmarks[j].x);
-                    inputArray.Add(landmark.handLandmarks[0].landmarks[j].y);
+                        inputArray.Add(landmark.handLandmarks[0].landmarks[j].x);
+                        inputArray.Add(landmark.handLandmarks[0].landmarks[j].y);
+                    }
+                }
+
+                var midpoint = bufferedResults[bufferedResults.Count / 2];
+
+                for (int i = 0; i < Config.NUM_INPUT_FRAMES - bufferedResults.Count; i++)
+                {
+                    for (int j = 0; j < Config.NUM_INPUT_POINTS; j++)
+                    {
+                        if (midpoint.handLandmarks.Count <= 0 || midpoint.handLandmarks[0].landmarks.Count <= 0)
+                            return; // Exit if no landmark is present
+
+                        inputArray.Add(midpoint.handLandmarks[0].landmarks[j].x);
+                        inputArray.Add(midpoint.handLandmarks[0].landmarks[j].y);
+                    }
+                }
+            }
+            else if (bufferedResults.Count >= 60)
+            {
+                var lastFrames = bufferedResults.GetRange(bufferedResults.Count - Config.NUM_INPUT_FRAMES, Config.NUM_INPUT_FRAMES);
+                foreach (var landmark in lastFrames)
+                {
+                    for (int j = 0; j < Config.NUM_INPUT_POINTS; j++)
+                    {
+                        if (landmark.handLandmarks == null || landmark.handLandmarks.Count <= 0 || landmark.handLandmarks[0].landmarks.Count <= 0)
+                            return; // Exit if no landmark is present
+
+                        inputArray.Add(landmark.handLandmarks[0].landmarks[j].x);
+                        inputArray.Add(landmark.handLandmarks[0].landmarks[j].y);
+                    }
                 }
             }
 
-            var midpoint = bufferedResults[bufferedResults.Count / 2];
-
-            for (int i = 0; i < Config.NUM_INPUT_FRAMES - bufferedResults.Count; i++)
+            if (inputArray.Count > 0)
             {
-                for (int j = 0; j < Config.NUM_INPUT_POINTS; j++)
-                {
-                    if (midpoint.handLandmarks.Count <= 0 || midpoint.handLandmarks[0].landmarks.Count <= 0)
-                        return; // Exit if no landmark is present
+                bool updated = true;
 
-                    inputArray.Add(midpoint.handLandmarks[0].landmarks[j].x);
-                    inputArray.Add(midpoint.handLandmarks[0].landmarks[j].y);
-                }
+                Debug.Log("Fingerspell: Running");
+                string translation = recognizer.RunModel(inputArray.ToArray());
+                lastTranslation = translation;
+
+                Debug.Log(translation);
+
             }
-        }
-        else if (bufferedResults.Count >= 60)
-        {
-            var lastFrames = bufferedResults.GetRange(bufferedResults.Count - Config.NUM_INPUT_FRAMES, Config.NUM_INPUT_FRAMES);
-            foreach (var landmark in lastFrames)
-            {
-                for (int j = 0; j < Config.NUM_INPUT_POINTS; j++)
-                {
-                    if (landmark.handLandmarks == null || landmark.handLandmarks.Count <= 0 || landmark.handLandmarks[0].landmarks.Count <= 0)
-                        return; // Exit if no landmark is present
 
-                    inputArray.Add(landmark.handLandmarks[0].landmarks[j].x);
-                    inputArray.Add(landmark.handLandmarks[0].landmarks[j].y);
-                }
-            }
-        }
-
-        if (inputArray.Count > 0)
-        {
-            Debug.Log("Fingerspell: Running");
-            recognizer.RunModel(inputArray.ToArray());
-        }
         });
+
     }
-    private static float[] convertLandmarkToFloatArray(NormalizedLandmarkList landmarks, ClassificationList handedness) {
+
+    void TriggerOnClick()
+    {
+
+    }
+
+
+    private static float[] convertLandmarkToFloatArray(NormalizedLandmarkList landmarks, ClassificationList handedness)
+    {
         float[] currentFrame = new float[42];
-        for (int i = 0; i < landmarks.Landmark.Count; i++) {
-            if (handedness.Classification[0].Label.Contains("Left")) {
+        for (int i = 0; i < landmarks.Landmark.Count; i++)
+        {
+            if (handedness.Classification[0].Label.Contains("Left"))
+            {
                 currentFrame[i * 2] = flip(landmarks.Landmark[i].Y);
             }
-            else {
+            else
+            {
                 currentFrame[i * 2] = landmarks.Landmark[i].Y;
             }
             currentFrame[i * 2 + 1] = flip(landmarks.Landmark[i].X);
@@ -109,7 +152,12 @@ public class SimpleSLR : MonoBehaviour {
         }
         return currentFrame;
     }
-    private static float flip(float original) {
+
+
+    private static float flip(float original)
+    {
         return 1.0f - original;
     }
+
+
 }
